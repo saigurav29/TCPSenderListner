c53fbaa9923145235c3336f4477b9b34db8c8bbf
saigurav29 <saiprasadgurav34@gmail.com>
2022-08-29 21:17:10 +0530

    added projects

---
+++ tcpSender/tcpSender/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace tcpSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            tcpsend();
+        }
+
+        public static void tcpsend()
+        {
+            IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+            string HostName = localIPs[3].ToString();
+            int port = 1690;
+            TcpClient tc = new TcpClient("127.0.0.1", port);
+            string filepath = "C:\\Users\\Sai prasad\\source\\repos\\tcpSender\\tcpSender\\tc.txt";
+            NetworkStream ns = tc.GetStream();
+            FileStream fs = new FileStream(filepath, FileMode.Open);
+            int data = fs.ReadByte();
+            while (data != -1)
+            {
+                ns.WriteByte((byte)data);
+                Console.WriteLine((byte)data);
+                data = fs.ReadByte();
+            }
+            Console.WriteLine("done");
+
+            fs.Close();
+            ns.Close();
+            tc.Close();
+        }
+    }
+}
---
+++ tcpreciver/tcpreciver/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace tcpreciver
+{
+    class Program
+    {
+        public delegate void testDelegate(string str);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Thread t = new Thread(new ThreadStart(ListenData));
+            t.Start();
+        }
+
+        public static void ListenData()
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int prt = 1690;
+            TcpListener tl = new TcpListener(ip, prt);
+            tl.Start();
+            TcpClient tc = tl.AcceptTcpClient();
+            NetworkStream ns = tc.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            string result = sr.ReadToEnd();
+
+            //Invoke(new testDelegate(print), new object[] { result });
+            Console.WriteLine(result);
+
+            tc.Close();
+            tl.Stop();
+        }
+
+        private static void print(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
